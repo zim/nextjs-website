2c60b3170c7ca3a8e3bfb534770633217bef42c6
Zim Segal <zimsegal@hotmail.com>
2022-06-25 15:15:42 +0100

    Add data folder and some basic teams stuff

--- pages/index.js
+++ pages/index.js
@@ -13,9 +13,8 @@ export default function Home({ allPostsData }) {
 			</Head>
 			<section className={utilStyles.headingMd}>
 				<p>
-					Hello, I'm <strong>Jolyon</strong>. I'm a software engineer and a
-					translator (English/Japanese). You can contact me on{" "}
-					<a href="https://twitter.com/chibicode">Twitter</a>.
+					Hello, I'm <strong>Jolyon</strong>. I'm a Web Developer. You can
+					contact me on <a href="https://twitter.com/chibicode">Twitter</a>.
 				</p>
 				<p>
 					(This is a sample website - you'll be building a site like this in{" "}
